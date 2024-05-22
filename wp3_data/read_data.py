e3a1bfa52febf6822ab341f86ace5c62703dea4e
Alex <alexfidalgo10@gmail.com>
2024-05-21 23:24:54 -0300

    .

--- wp3_data/read_data.py
+++ wp3_data/read_data.py
@@ -22,7 +22,9 @@ def read_mat_with_scipy(file):
 
 cluster_region = read_mat_with_scipy("clusters_regionwise") # The file "clusters_regionwise.mat" contains the information about the clustering for each region
 
-cluster_region101 = cluster_region['c101']
+cluster_region101 = cluster_region['c101'] # contains 20 rows (variables) and 8 columns (regions)
+
+print(f"len(cluster_region101): {len(cluster_region101)}")
 
 print(f"len(cluster_region101[2][0]): {len(cluster_region101[2][0])}") # number of clusters for precipitation (3rd variable, index 2) in the 1st region (index 0)
 
