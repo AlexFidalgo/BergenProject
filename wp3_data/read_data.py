113bff44f64e1d8936d1aaf97a8be32ce2036db6
Alex <alexfidalgo10@gmail.com>
2024-05-22 19:19:28 -0300

    removes h5py library

--- wp3_data/read_data.py
+++ wp3_data/read_data.py
@@ -1,7 +1,6 @@
 import scipy.io
 import os
 import numpy as np
-import h5py
 
 def get_current_dir():
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -11,14 +10,6 @@ def read_mat_with_scipy(file):
 
     mat_files_folder = os.path.join(get_current_dir(), 'mat_files')
     return scipy.io.loadmat(os.path.join(mat_files_folder, 'clusters_regionwise'+'.mat'))
-
-# def read_mat_with_h5py(file):
-
-#     mat_files_folder = os.path.join(get_current_dir(), 'mat_files')
-#     f = h5py.File(os.path.join(mat_files_folder, file+'.mat'),'r')
-#     # data = f.get('data/variable1')
-#     # data = np.array(data) # For converting to a NumPy array
-# a = read_mat_with_h5py('clusters_regionwise')
 
 cluster_region = read_mat_with_scipy("clusters_regionwise") # The file "clusters_regionwise.mat" contains the information about the clustering for each region
 
