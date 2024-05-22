6c523e7511a6d79a16437f8d2a568e1f94223886
Alex <alexfidalgo10@gmail.com>
2024-05-21 23:23:05 -0300

    reads data using scipy.io

--- wp3_data/read_data.py
+++ wp3_data/read_data.py
@@ -12,16 +12,18 @@ def read_mat_with_scipy(file):
     mat_files_folder = os.path.join(get_current_dir(), 'mat_files')
     return scipy.io.loadmat(os.path.join(mat_files_folder, 'clusters_regionwise'+'.mat'))
 
-def read_mat_with_h5py(file):
+# def read_mat_with_h5py(file):
 
-    mat_files_folder = os.path.join(get_current_dir(), 'mat_files')
-    f = h5py.File(os.path.join(mat_files_folder, file+'.mat'),'r')
-    # data = f.get('data/variable1')
-    # data = np.array(data) # For converting to a NumPy array
+#     mat_files_folder = os.path.join(get_current_dir(), 'mat_files')
+#     f = h5py.File(os.path.join(mat_files_folder, file+'.mat'),'r')
+#     # data = f.get('data/variable1')
+#     # data = np.array(data) # For converting to a NumPy array
+# a = read_mat_with_h5py('clusters_regionwise')
 
-# clusters_regionwise = read_mat_with_scipy('clusters_regionwise')
-# gr_europe = read_mat_with_scipy('gr_Europe')
+cluster_region = read_mat_with_scipy("clusters_regionwise") # The file "clusters_regionwise.mat" contains the information about the clustering for each region
 
-a = read_mat_with_h5py('clusters_regionwise')
+cluster_region101 = cluster_region['c101']
 
-x = 1
+print(f"len(cluster_region101[2][0]): {len(cluster_region101[2][0])}") # number of clusters for precipitation (3rd variable, index 2) in the 1st region (index 0)
+
+print(f"len(cluster_region101[9][0]): {len(cluster_region101[9][0])}") # number of clusters for temperature (10th variable, index 9) in the 1st region
